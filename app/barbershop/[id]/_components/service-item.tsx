d7d152cfda7b5caef6e740c462dd73fbfdeb2698
rweventos <rweventos.rj@gmail.com>
2024-02-03 21:01:44 -0300

    fix: modify css variables to optimize non-mobile screens

--- app/barbershop/[id]/_components/service-item.tsx
+++ app/barbershop/[id]/_components/service-item.tsx
@@ -8,7 +8,7 @@ type ServiceItemProps = {
 
 const ServiceItem = ({ service }: ServiceItemProps) => {
   return (
-    <div className="flex items-center justify-between gap-4 rounded-lg bg-muted p-2 shadow-md">
+    <div className="flex items-center justify-between gap-4 rounded-lg bg-muted p-2 shadow-md lg:justify-normal">
       <div className="relative h-[100px] w-[100px] overflow-hidden rounded-lg">
         <Image
           src={service.imageUrl}
@@ -17,10 +17,14 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
           objectFit="cover"
         />
       </div>
-      <div className="flex flex-col">
-        <h2 className="text-sm font-bold">{service.name}</h2>
-        <p className="text-xs text-muted-foreground">{service.description}</p>
-        <div className="mt-2 flex items-center justify-between">
+      <div className="flex flex-col lg:w-full lg:flex-row lg:items-center lg:justify-between">
+        <div className="flex flex-col">
+          <h2 className="text-sm font-bold lg:text-lg">{service.name}</h2>
+          <p className="text-xs text-muted-foreground lg:text-base">
+            {service.description}
+          </p>
+        </div>
+        <div className="mt-2 flex items-center justify-between lg:mt-0 lg:flex-col lg:space-y-2">
           <p className="text-primary">R$ {service.price.toFixed(2)} </p>
           <Button size="sm">Agendar</Button>
         </div>
